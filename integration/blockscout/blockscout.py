38dfd656e1aa7505f650dbe9de98d95d1910efba
Petr Kozhevnikov <kozhevnikov.peter@gmail.com>
2021-01-22 12:52:02 +0300

    WIP

--- integration/blockscout/blockscout.py
+++ integration/blockscout/blockscout.py
@@ -12,10 +12,12 @@ if __name__ == "__main__":
     with open(args.file, 'r') as content_file:
         content = content_file.read()
 
-        deployed_contracts = re.findall( r'([a-z,A-Z,0-9]*) deployed at: (.*)0x([0-9,x,a-z,A-Z]*)', content)
+        deployed_contracts = re.findall( r'Nonce: ([0-9]*)\n(.*)Block: ([0-9]*)(.*)\n(.*) deployed at: (.*)0x([0-9,x,a-z,A-Z]*)', content)
         for contract in deployed_contracts:
-            address = contract[2]
-            name = contract[0]
+            nonce = contract[0]
+            block = contract[2]
+            name = contract[4].replace(' ', '')
+            address = contract[6]
 
             with open('./build/contracts/{}.json'.format(name), 'r') as abi_content:
                 abi = json.loads(abi_content.read())
@@ -23,7 +25,15 @@ if __name__ == "__main__":
                 contract_name = abi['contractName']
                 compiler_version = abi['compiler']['version']
                 source = abi['source'].replace('\n', '\\n')
+                byte_code = abi['bytecode']
                 abi_str = json.dumps(abi['abi'], separators=(',', ':'))
+
+                sql += '''
+                insert into public.addresses (fetched_coin_balance, fetched_coin_balance_block_number, hash, contract_code, inserted_at, updated_at, nonce, decompiled, verified) values
+                (0,{},'''.format(block)
+                sql += "E'\\\\x{}',".format(address)
+                sql += "E'\\\\x{}',".format(byte_code[2:])
+                sql += "now(), now(), null, false, false);"
 
                 sql += '''
                 insert into public.smart_contracts
