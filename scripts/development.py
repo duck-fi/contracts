03483ca103c0825584bafc082c57ba5aaaf536e8
Petr Kozhevnikov <kozhevnikov.peter@gmail.com>
2021-01-23 13:17:55 +0300

    WIP

--- scripts/development.py
+++ scripts/development.py
@@ -1,6 +1,7 @@
 from brownie import (
     project,
     accounts,
+    ERC20,
     FarmToken,
     StakableERC20,
     Minter,
@@ -10,6 +11,7 @@ from brownie import (
     SimpleVotingStrategy,
 )
 from pathlib import Path
+from . import utils
 
 
 DEPLOYER = accounts[2]
@@ -18,41 +20,60 @@ WEEK = DAY * 7
 
 
 def deploy():
-    usdn_token = StakableERC20.deploy("Neutrino USD", "USDN", 18, {'from': DEPLOYER})
-    farm_token = FarmToken.deploy("Dispersion Farming Token", "DFT", 18, 1000, {'from': DEPLOYER})
+    usdt = deploy_tether()
+    usdn = deploy_usdn()
+    dft = FarmToken.deploy(
+        "Dispersion Farming Token", "DFT", 18, 20_000, {'from': DEPLOYER})
+
+    # Uniswap
+    uniswap_factory = deployUniswapFactory()
+    uniswap_factory.createPair(usdn, usdt) # USDN/USDT
 
 #     deployCurveContracts()
-    deployUniswapContracts()
+    # deployUniswapContracts()
 
-    minter = Minter.deploy(farm_token, {'from': DEPLOYER})
-    farm_token.setMinter(minter, {'from': DEPLOYER})
+    # minter = Minter.deploy(farm_token, {'from': DEPLOYER})
+    # farm_token.setMinter(minter, {'from': DEPLOYER})
 
-    reaper_controller = ReaperController.deploy(minter, {'from': DEPLOYER})
-    minter.setReaperController(reaper_controller, {'from': DEPLOYER})
+    # reaper_controller = ReaperController.deploy(minter, {'from': DEPLOYER})
+    # minter.setReaperController(reaper_controller, {'from': DEPLOYER})
 
-    # Voting
-    voting_controller = VotingController.deploy(
-        reaper_controller, {'from': DEPLOYER})
-    simple_voting_strategy = SimpleVotingStrategy.deploy(
-        farm_token, 1, DAY, {'from': DEPLOYER})
-    voting_controller.setStrategy(
-        farm_token, simple_voting_strategy, {'from': DEPLOYER})
+    # # Voting
+    # voting_controller = VotingController.deploy(
+    #     reaper_controller, {'from': DEPLOYER})
+    # simple_voting_strategy = SimpleVotingStrategy.deploy(
+    #     farm_token, 1, DAY, {'from': DEPLOYER})
+    # voting_controller.setStrategy(
+    #     farm_token, simple_voting_strategy, {'from': DEPLOYER})
 
     # Reapers
     # uniswapReaper = Reaper.deploy(_lp_token: address, reaper_controller, voting_controller, {'from': DEPLOYER})
     # minter.setReaperController(reaperController, {'from': DEPLOYER})
 
 
-def deployCurveContracts():
-    package = 'curvefi/curve-dao-contracts@1.0.0'
-    aaa = project.load(Path.home().joinpath(".brownie").joinpath(f"packages/curvefi/curve-dao-contracts@1.1.0"), 'curvefi/curve-dao-contracts@1.1.0').ERC20CRV
-    crvToken = pm('curvefi/curve-dao-contracts@1.0.0').deploy({'from': DEPLOYER})
-    assert crvToken.total_supply() == 0
+def deploy_tether():
+    tether_token = ERC20.deploy(
+        "Tether USD", "USDT", 6, 0, {'from': DEPLOYER})
+    for account in accounts:
+        tether_token.mint(account, 1_000_000 * 10 ** 6, {'from': DEPLOYER})
+    return tether_token
 
 
-def deployUniswapContracts():
-    package = 'Uniswap/uniswap-v2-core@1.0.1'
-    uniswapProject = project.load(Path.home().joinpath(".brownie").joinpath(f"packages/Uniswap/uniswap-v2-core@1.0.1"), 'Uniswap/uniswap-v2-core@1.0.1')
-    uniswapV2ERC20Token = uniswapProject.UniswapV2ERC20.deploy({'from': DEPLOYER})
-#     uniswapV2Factory = uniswapProject.UniswapV2Factory.deploy(DEPLOYER, {'from': DEPLOYER})
-#     pairToken = uniswapV2Factory.createPair(tokenA, tokenB)
+def deploy_usdn():
+    usdn_token = StakableERC20.deploy(
+        "Neutrino USD", "USDN", 18, {'from': DEPLOYER})
+    for account in accounts:
+        usdn_token.deposit(account, 1_000_000 * 10 ** 18, {'from': DEPLOYER})
+    return usdn_token
+
+
+def deployUniswapFactory():
+    uniswap = utils.load_package('Uniswap/uniswap-v2-core@1.0.1')
+    return uniswap.UniswapV2Factory.deploy(DEPLOYER, {'from': DEPLOYER})
+
+
+# def deployCurveContracts():
+#     package = 'curvefi/curve-dao-contracts@1.0.0'
+#     aaa = project.load(Path.home().joinpath(".brownie").joinpath(f"packages/curvefi/curve-dao-contracts@1.1.0"), 'curvefi/curve-dao-contracts@1.1.0').ERC20CRV
+#     crvToken = pm('curvefi/curve-dao-contracts@1.0.0').deploy({'from': DEPLOYER})
+#     assert crvToken.total_supply() == 0
